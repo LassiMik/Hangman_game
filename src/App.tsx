6acf8616174ad6691dc652aa9bd232252bf6181c
LassiMik <112076377+LassiMik@users.noreply.github.com>
2023-05-02 18:22:42 +0300

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
 import { useState, useCallback } from 'react'
 import words from './wordList.json'
-import { HangmanDrawing } from './HangmanDrawing';
-import { HangmanWord } from './HangmanWord';
-import { Keyboard } from './Keyboard';
+import { HangmanDrawing } from './components/Drawing';
+import { HangmanWord } from './components/Word';
+import { Keyboard } from './components/Keyboard';
+import Popup from './components/Popup'
+import styles from './App.module.css'
 
 function getWord() {
-  return words[Math.floor(Math.random() * words.length)]
+  return words[Math.floor(Math.random() * words.length)];
 }
 
 function App() {
-  
-  const [guessWord, setGuessWord] = useState(getWord) 
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([ ])
+
+  const [guessWord, setGuessWord] = useState(getWord)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
   const incorrectLetters = guessedLetters.filter(
     letter => !guessWord.includes(letter)
   )
-
-  function getNewWord() {
-    setGuessWord(getWord())
-    setGuessedLetters([])
-  }
-  
 
   const addGuessedLetter = useCallback((key: string) => {
     if (guessedLetters.includes(key)) return
@@ -31,41 +27,32 @@ function App() {
 
   //6 bodyparts
   const isLoser = incorrectLetters.length >= 6
-  const isWinner = guessWord.split('').every(letter => 
+  const correctLetters = guessWord.split('').every(letter =>
     guessedLetters.includes(letter))
 
-  console.log(guessWord);
+  function getNewWord() {
+
+    setGuessWord(getWord())
+    setGuessedLetters([])
+  }
+
   return (
-    <div style={{
-      maxWidth: '800px',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '2rem',
-      margin: '0 auto',
-      alignItems: 'center'
-     }}>
-      <div style={{
-        fontSize: '2rem',
-        textAlign: 'center'
-      }}>
-        {isWinner && 'Winner!'}
-        {isLoser && 'Loser!'}
-      
+    <div className={styles.gamecontainer}>
+      <div className={styles.gamecontainerchild}>
       </div>
-      <div>
-        <button onClick={() => getNewWord()}>Reset</button>
-      </div>
-      <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
-      <HangmanWord reveal={isLoser} guessedLetters = { guessedLetters } guessWord = { guessWord } />
-      <div style={{ alignSelf: 'stretch'}}>
-        <Keyboard 
-        disabled={isWinner || isLoser}
-        activeLetters={guessedLetters.filter(letter => 
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord reveal={isLoser} guessedLetters={guessedLetters} guessWord={guessWord} />
+      {isLoser && <Popup correctLetters={correctLetters} wrongLetters={incorrectLetters} selectedWord={guessWord} playAgain={getNewWord} />}
+      {correctLetters && <Popup correctLetters={correctLetters} wrongLetters={incorrectLetters} selectedWord={guessWord} playAgain={getNewWord} />}
+      <div style={{ alignSelf: 'stretch' }}>
+        <Keyboard
+          disabled={correctLetters || isLoser}
+          activeLetters={guessedLetters.filter(letter =>
             guessWord.includes(letter)
           )}
           inactiveLetters={incorrectLetters}
           addGuessedLetter={addGuessedLetter}
-          />
+        />
       </div>
     </div>
   )
